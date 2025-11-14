2d1b0f420ef113e4ad6b38eb31dcc48feb3be5b7
Francisco Javier Trapero Graciano <javitrapero@MacBook-Air-de-Francisco.local>
2025-11-14 22:20:06 +0100

    subida

---
+++ prueba.py
@@ -0,0 +1,10 @@
+print("Hola mundo")
+
+print("Esta es una prueba de un archivo Python.")
+
+
+print("¡Python es genial!")
+print("Estoy aprendiendo a programar en Python.")
+print("¡Vamos a crear algo increíble!")
+
+
